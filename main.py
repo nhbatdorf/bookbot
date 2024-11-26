7a0d8a2db358e31ae4d2e204011ef583922443f1
nhbatdorf <nhbatdorf@gmail.com>
2024-11-26 03:56:27 -0500

    report function completed

--- main.py
+++ main.py
@@ -21,10 +21,22 @@ def count_char(text):
             count_dict[character] = 1
     return count_dict
 
+def sort_on(dict):
+    return dict["num"]
+
 def report(path, text):
-    
+    char_count = count_char(text)
+    char_dict = []
+    for char in char_count:
+        if char.isalpha():
+            new_entry = {"char": char, "num": char_count[char]}
+            char_dict.append(new_entry)
+    char_dict.sort(reverse=True, key=sort_on)
+
     print(f"--- Reporting on {path} ---")
     print(f"{word_count(text)} words found in the document")
+    for char in char_dict:
+        print(f"The '{char["char"]}' character was found {char["num"]} times")
     print("--- End Report ---")
 
 main()
